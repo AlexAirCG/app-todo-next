8d7b78d20cc28a0795f92958d128a46eb684590c
AlexAirCG <alexairggg@yandex.ru>
2025-05-23 14:20:05 +0300

    change route

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -8,26 +8,14 @@ const prisma = new PrismaClient();
 
 export const authOptions = {
   adapter: PrismaAdapter(prisma),
-  // Configure one or more authentication providers
   providers: [
-    // ...add more providers here
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
-
     EmailProvider({
       server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
-      // server: {
-      //   host: process.env.EMAIL_SERVER_HOST,
-      //   port: process.env.EMAIL_SERVER_PORT,
-      //   auth: {
-      //     user: process.env.EMAIL_SERVER_USER,
-      //     pass: process.env.EMAIL_SERVER_PASSWORD,
-      //   },
-      // },
-      // from: process.env.EMAIL_FROM,
     }),
   ],
 };

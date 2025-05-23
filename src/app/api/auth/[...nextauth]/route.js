64769c2e698e334bc3793d95aba80a3e772ddeec
AlexAirCG <alexairggg@yandex.ru>
2025-05-23 13:48:07 +0300

    change route and env

--- src/app/api/auth/[...nextauth]/route.js
+++ src/app/api/auth/[...nextauth]/route.js
@@ -17,15 +17,17 @@ export const authOptions = {
     }),
 
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD,
-        },
-      },
+      server: process.env.EMAIL_SERVER,
       from: process.env.EMAIL_FROM,
+      // server: {
+      //   host: process.env.EMAIL_SERVER_HOST,
+      //   port: process.env.EMAIL_SERVER_PORT,
+      //   auth: {
+      //     user: process.env.EMAIL_SERVER_USER,
+      //     pass: process.env.EMAIL_SERVER_PASSWORD,
+      //   },
+      // },
+      // from: process.env.EMAIL_FROM,
     }),
   ],
 };
